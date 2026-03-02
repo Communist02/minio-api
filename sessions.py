6ca9fabb9caf9642aa1edbbe8abb12f80d926880
Denis Mazur <kilerden2002@gmail.com>
2026-03-02 17:39:08 +1000

    Update

--- sessions.py
+++ sessions.py
@@ -6,13 +6,15 @@ import config
 async def get_session(token: str) -> dict | None:
     async with httpx.AsyncClient(verify=not config.debug_mode) as client:
         response = await client.get(
-            f'{config.auth_api_url}/service_session',
-            headers={"token": token},
+            f'{config.auth_api_url}/introspect',
+            headers={'Authorization': f'Beaver {token}'},
         )
     if response.status_code == 200:
         session = response.json()
-        session['hash1'] = base64.urlsafe_b64decode(session['hash1'].encode())
-        return session
+        if session['active'] == True:
+            session['hash1'] = base64.urlsafe_b64decode(session['hash1'].encode())
+            session['jwt_token'] = session['jwt']
+            return session
 
 
 async def delete_session(token: str) -> None:
